527926ff22184a2f25947c1d355008186e15f0b4
n0P7xJ <program79766971@gmail.com>
2026-09-11 00:37:17 +0200

    Complete lesson 1: extract quotes and authors from quotes.toscrape.com

--- lesson_01_quotes/scraper.py
+++ lesson_01_quotes/scraper.py
@@ -2,17 +2,21 @@ from playwright.sync_api import sync_playwright
 
 URL = "http://quotes.toscrape.com"
 
-
 def main():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(URL)
 
-        # TODO: знайти всі елементи .quote
-        # TODO: для кожного вивести текст (.text) і автора (.author)
-        # TODO: порахувати і вивести загальну кількість
-
+        quotes = page.locator(".quote").all()
+        
+        for quote in quotes:
+            text = quote.locator(".text").text_content()
+            author = quote.locator(".author").text_content()
+            print(f"{text} - {author}")
+            
+        print(f"Count of quotes:{len(quotes)}")
+        
         browser.close()
 
 
